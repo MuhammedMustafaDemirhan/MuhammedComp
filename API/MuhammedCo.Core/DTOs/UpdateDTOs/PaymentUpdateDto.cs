9292ac1a379801912ed704d45e53b86771692fd0
Muhammed Mustafa Demirhan <muhammedmustafademirhan@gmail.com>
2025-11-09 15:40:07 +0300

    feat(dto): added update DTOs for entities

---
+++ API/MuhammedCo.Core/DTOs/UpdateDTOs/PaymentUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuhammedCo.Core.DTOs.UpdateDTOs
+{
+    public class PaymentUpdateDto
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public double Amount { get; set; }
+    }
+}
